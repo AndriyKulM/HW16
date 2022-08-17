c1c5c75b70641958c7d3ceba21a2f9254f493db4
Andriy Kulyk <andinat@i.ua>
2022-08-17 15:45:15 +0300

    corrected HW16

--- hw-regex.py
+++ hw-regex.py
@@ -1,17 +1,15 @@
-from ipaddress import ip_address
 import re
 
 
 # 1. Search a phone numbers
 
-pattern1 = r"\d{1,3}[-]\d{1,3}[-]\d{1,4}"          # \d - all digits
-text1 = "Hello, my phone number is 251-65-23."
+pattern1 = r"(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)"        
+text1 = "Hello, my phone number is 38-067-666-66-66."
 
-result_phone = re.findall(pattern1, text1)
+result_phone = re.search(pattern1, text1)
 
 if result_phone:
-    print(result_phone)
-    print("Phone number found!")
+    print("Phone number found! :", result_phone[0])
 else:
     print("Phone number not found!")
 
@@ -29,8 +27,7 @@ text2 = "John.Smith_1234@gmail.com"
 result_email = re.match(pattern2, text2)
 
 if result_email:
-    print(result_email[0])
-    print("Email is valid!")
+    print("Email is valid! :", result_email[0])
 else:
     print("Wrong email!")
 
@@ -42,7 +39,7 @@ octets = re.split(r"\.", text3)               # breaking the ip address into oct
 octets = [int(octet) for octet in octets]     # convert to an integer to remove redundant zeros
 octets = [str(octet) for octet in octets]     # back to string
 result_rrz = ".".join(octets)                 # and join octets into ip address by separator '.'
-print(result_rrz)
+print("ip address without zeros:", result_rrz)
 
 
 # 4. Check if IP address is valid
@@ -53,6 +50,6 @@ pattern3 = r"^\d+\.\d+\.\d+\.\d+$"
 # check whether the octet is in the range from 0 to 255 and whether there are 4 octets
 result_ip = [0<=int(octet)<256 for octet in re.split("\.", re.match(pattern3, ip_address_).group(0))].count(True)==4
 if result_ip==True:
-    print("ip address is valid!")
+    print("ip address is valid! :", ip_address_)
 else:
     print("invalid ip address")
